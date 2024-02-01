4f6895d8adbef1f53e74f289ca3242fa021d5d01
andrewrodgers90 <132149730+andrewrodgers90@users.noreply.github.com>
2024-02-02 00:50:11 +0700

    Update ft_str_is_numeric.c
    Remove brackets in 'if / else' conditions.
    
    Remove test functions.
    
    Remove function prototype.

--- ft_str_is_numeric.c
+++ ft_str_is_numeric.c
@@ -12,37 +12,14 @@
 
 #include <unistd.h>
 
-int	ft_str_is_numeric(char *str);
-
 int	ft_str_is_numeric(char *str)
 {
 	while (*str != '\0')
 	{
 		if (*str >= 48 && *str <= 57)
-		{
 			str++;
-		}
 		else
-		{
 			return (0);
-		}
 	}
 	return (1);
 }
-/*
-int	main(void)
-{
-	char	arr[] = "H3ll0";
-	char	arr2[] = "12345";
-	char	arr_empty[] = "";
-	int	i = ft_str_is_numeric(arr);
-	int	j = ft_str_is_numeric(arr2);
-	int	k = ft_str_is_numeric(arr_empty);
-
-	printf("Arr returns value of: %d\n", i);
-	printf("Arr2 returns value of: %d\n", j);
-	printf("Arr_empty returns value of: %d\n", k);
-
-	return (0);
-}
-*/
