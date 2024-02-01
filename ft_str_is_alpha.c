e8cbcd91bdfc6bc1a64965a690da789696aeeced
andrewrodgers90 <132149730+andrewrodgers90@users.noreply.github.com>
2024-02-02 00:51:16 +0700

    Update ft_str_is_alpha.c
    Remove brackets in 'if / else' conditions.
    
    Remove test functions.
    
    Remove function prototype.

--- ft_str_is_alpha.c
+++ ft_str_is_alpha.c
@@ -12,8 +12,6 @@
 
 #include <unistd.h>
 
-int	ft_str_is_alpha(char *str);
-
 int	ft_str_is_alpha(char *str)
 {
 	int	i;
@@ -22,32 +20,11 @@ int	ft_str_is_alpha(char *str)
 	while (str[i] != '\0')
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-		{
 			i++;
-		}
 		else if (str[i] >= 'A' && str[i] <= 'Z')
-		{
 			i++;
-		}
 		else
-		{
 			return (0);
-		}
 	}
 	return (1);
 }
-/*
-int	main(void)
-{
-	char	arr[] = "Hello";
-	char	arr2[] = "H3ll0";
-	char	arr_empty[] = "";
-	int	i = ft_str_is_alpha(arr);
-	int	j = ft_str_is_alpha(arr2);
-	int	k = ft_str_is_alpha(arr_empty);
-
-	printf("Arr returns value of: %d\n", i);
-	printf("Arr2 returns value of: %d\n", j);
-	printf("Arr_empty returns value of: %d\n", k);
-}
-*/
