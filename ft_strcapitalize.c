0971569f9fde9f20b3457fbfa57f5a0e90f1c990
andrewrodgers90 <132149730+andrewrodgers90@users.noreply.github.com>
2024-02-02 00:47:24 +0700

    Update ft_strcapitalize.c
    Remove brackets in 'if / else' conditions.

--- ft_strcapitalize.c
+++ ft_strcapitalize.c
@@ -15,33 +15,21 @@
 int	ft_is_lowercase(char c)
 {
 	if (c >= 'a' && c <= 'z')
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
 
 int	ft_is_alphanum(char c)
 {
 	if (c >= 'a' && c <= 'z')
-	{
 		return (1);
-	}
 	if (c >= '0' && c <= '9')
-	{
 		return (1);
-	}
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
 
 char	*ft_strlower(char *str)
@@ -57,9 +45,7 @@ char	*ft_strlower(char *str)
 			count++;
 		}
 		else
-		{
 			count++;
-		}
 	}
 	return (str);
 }
@@ -73,24 +59,10 @@ char	*ft_strcapitalize(char *str)
 	while (str[i] != '\0')
 	{
 		if ((ft_is_alphanum(str[i - 1]) == 0) && (ft_is_lowercase(str[i]) == 1))
-		{
 			str[i] -= 32;
-		}
 		i++;
 	}
 	if (str[0] >= 'a' && str[0] <= 'z')
-	{
 		str[0] -= 32;
-	}
 	return (str);
 }
-/*
-int	main(void)
-{
-	char	arr[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
-	printf("Before: %s\n", arr);
-	ft_strcapitalize(arr);
-	printf("After: %s\n", arr);
-	return (0);
-}
-*/
