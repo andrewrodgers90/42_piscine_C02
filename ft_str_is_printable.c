4f6e724f38b43f0512627c910d96e4f97cd5962c
andrewrodgers90 <132149730+andrewrodgers90@users.noreply.github.com>
2024-02-02 00:49:20 +0700

    Update ft_str_is_printable.c
    Remove brackets in 'if / else' conditions.
    
    Remove test functions.
    
    Remove function prototype.

--- ft_str_is_printable.c
+++ ft_str_is_printable.c
@@ -12,38 +12,14 @@
 
 #include <unistd.h>
 
-int	ft_str_is_printable(char *str);
-
 int	ft_str_is_printable(char *str)
 {
 	while (*str != '\0')
 	{
 		if (*str >= 32 && *str <= 126)
-		{
 			str++;
-		}
 		else
-		{
 			return (0);
-		}
 	}
 	return (1);
 }
-/*
-int	main(void)
-{
-	char	arr[] = "hello";
-	char	arr2[] = "Hello";
-	char	arr3[] = "12345";
-	char	arr_empty[] = "";
-	int	i = ft_str_is_printable(arr);
-	int	j = ft_str_is_printable(arr2);
-	int	k = ft_str_is_printable(arr_empty);
-	int	l = ft_str_is_printable(arr3);
-
-	printf("Arr returns value of: %d\n", i);
-	printf("Arr2 returns value of: %d\n", j);
-	printf("Arr3 returns value of: %d\n", l);
-	printf("Arr_empty returns value of: %d\n", k);
-}
-*/
