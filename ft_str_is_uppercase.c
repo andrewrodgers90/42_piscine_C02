9daeb7a935efecb4fa649b176aaa5c0cc9bbc72c
andrewrodgers90 <132149730+andrewrodgers90@users.noreply.github.com>
2024-02-02 00:48:18 +0700

    Update ft_str_is_uppercase.c
    Remove brackets in 'if / else' conditions.
    
    Remove test functions.

--- ft_str_is_uppercase.c
+++ ft_str_is_uppercase.c
@@ -12,39 +12,14 @@
 
 #include <unistd.h>
 
-int	ft_str_is_uppercase(char *str);
-
 int	ft_str_is_uppercase(char *str)
 {
 	while (*str != '\0')
 	{
 		if (*str >= 65 && *str <= 90)
-		{
 			str++;
-		}
 		else
-		{
 			return (0);
-		}
 	}
 	return (1);
 }
-
-/*
-int	main(void)
-{
-	char	arr[] = "HELLO";
-	char	arr2[] = "Hello";
-	char	arr3[] = "12345";
-	char	arr_empty[] = "";
-	int	i = ft_str_is_uppercase(arr);
-	int	j = ft_str_is_uppercase(arr2);
-	int	k = ft_str_is_uppercase(arr_empty);
-	int	l = ft_str_is_uppercase(arr3);
-
-	printf("Arr returns value of: %d\n", i);
-	printf("Arr2 returns value of: %d\n", j);
-	printf("Arr3 returns value of: %d\n", l);
-	printf("Arr_empty returns value of: %d\n", k);
-}
-*/
